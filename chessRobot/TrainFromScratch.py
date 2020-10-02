9bdefc08466336b78700b21714e6deaa6edc7f2e
NSDuc <nguyensyduc97@gmail.com>
2020-10-02 23:07:03 +0700

    TrainFromScratch.py frame

--- chessRobot/TrainFromScratch.py
+++ chessRobot/TrainFromScratch.py
@@ -1,41 +1,75 @@
-# Generated with SMOP  0.41
-from libsmop import *
-# .\TrainFromScratch.m
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
 
-    clear
-    clc
-    ## Load and Explore the Image Data
-    digitDatasetPath=fullfile('C:\','aaa')
-# .\TrainFromScratch.m:3
-    digitData=imageDatastore(digitDatasetPath,'IncludeSubfolders',true,'LabelSource','foldernames')
-# .\TrainFromScratch.m:4
-    ## Check the number of images in each category.
-    CountLabel=digitData.countEachLabel
-# .\TrainFromScratch.m:8
-    ## Specify Training and Test Sets
-    trainingNumFiles=675
-# .\TrainFromScratch.m:11
-    rng(1)
-    trainDigitData,testDigitData=splitEachLabel(digitData,trainingNumFiles,'randomize',nargout=2)
-# .\TrainFromScratch.m:13
-    ## Change the network architecture
+
+clear;clc;
+%% Load and Explore the Image Data
+digitDatasetPath = fullfile('C:\\','aaa');
+digitData = imageDatastore(digitDatasetPath, ...
+    'IncludeSubfolders', true, 'LabelSource', 'foldernames');
+
+%% Check the number of images in each category.
+CountLabel = digitData.countEachLabel
+
+%% Specify Training and Test Sets
+trainingNumFiles = 675;
+rng(1) % For reproducibility
+np.random.seed(1)
+
+[trainDigitData,testDigitData] = splitEachLabel(digitData, ...
+    trainingNumFiles, 'randomize');
+
+%% Change the network architecture
+model = keras.Sequential (
+    keras.Input(shape=(90, 90, 1)),
+    layers.Conv2D(filters=16, kernel_size=3, padding='valid'),
+    layers.BatchNormalization(),
+    layers.ReLU(),
+
+    layers.MaxPooling2D(pool_size=(2, 2), strides=(3,3)),
+    layers.Conv2D(filters=16, kernel_size=3, padding='valid'),
+    layers.BatchNormalization(),
+    layers.ReLU(),
     
-    layers=concat([imageInputLayer(concat([90,90,1])),convolution2dLayer(3,16,'Padding',1),batchNormalizationLayer,reluLayer,maxPooling2dLayer(2,'Stride',2),convolution2dLayer(3,16,'Padding',1),batchNormalizationLayer,reluLayer,maxPooling2dLayer(2,'Stride',2),convolution2dLayer(3,32,'Padding',1),batchNormalizationLayer,reluLayer,fullyConnectedLayer(2),softmaxLayer,classificationLayer])
-# .\TrainFromScratch.m:18
-    # Specify training options
-    options=trainingOptions('sgdm','MaxEpochs',10,'MiniBatchSize',128,'InitialLearnRate',0.0001,'ExecutionEnvironment','cpu','Plots','training-progress','ValidationData',testDigitData,'ValidationFrequency',30)
-# .\TrainFromScratch.m:42
-    # Train the network
-    testtttt=trainNetwork(trainDigitData,layers,options)
-# .\TrainFromScratch.m:53
+    layers.MaxPooling2D(pool_size=(2, 2), strides=(3,3)),
+    layers.Conv2D(filters=32, kernel_size=3, padding='valid'),
+    layers.BatchNormalization(),
+    layers.ReLU(),
     
-    # Classify the Images in the Test Data and Compute Accuracy
-    predictedLabels=classify(testtttt,testDigitData)
-# .\TrainFromScratch.m:56
-    valLabels=testDigitData.Labels
-# .\TrainFromScratch.m:57
-    # Calculate the accuracy.
-    accuracy=sum(predictedLabels == valLabels) / numel(valLabels)
-# .\TrainFromScratch.m:60
-    #Save Network
-    save('testtttt')
+    layers.Dense(units=2),
+    layers.Softmax(),
+    layers.Dense(),
+)
+
+model.compile(
+    loss='categorical_crossentropy', 
+    optimizer='adam', 
+    metrics=['accuracy']
+    )
+   
+model.fit()
+
+options = trainingOptions(...
+    'sgdm',...
+    'MaxEpochs', 10, ...
+    'MiniBatchSize', 128,...
+    'InitialLearnRate', 0.0001,...
+    'ExecutionEnvironment', 'cpu',... 
+    'Plots', 'training-progress',...
+    'ValidationData', testDigitData,...
+    'ValidationFrequency', 30);
+
+% Train the network
+testtttt = trainNetwork(trainDigitData, layers, options); % convnet2
+
+% Classify the Images in the Test Data and Compute Accuracy
+predictedLabels  = classify(testtttt, testDigitData); 
+valLabels  = testDigitData.Labels;
+
+% Calculate the accuracy.
+accuracy = sum(predictedLabels == valLabels)/numel(valLabels)
+
+%Save Network
+save testtttt
