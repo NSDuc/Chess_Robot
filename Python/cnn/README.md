2f53e7c73291a3412c60e7f4c1a172fe6fbeaa68
NSDuc <nguyensyduc97@gmail.com>
2020-11-04 23:50:04 +0700

    pytorch: new model to increase acc

--- Python/cnn/README.md
+++ Python/cnn/README.md
@@ -21,3 +21,28 @@ red2.pth : train with data_red_2 + 20 epoch
 
 black.pth  : train with data_black + 10 epoch
 black1.pth : train with data_black + 20 epoch
+
+=========================================================================================================
+Hướng dẫn chạy file train.py:
+
+
+MODEL_CLASS: mạng cnn được sử dụng (Net hoặc Net2), Net2 ok hơn.
+COLOR: red/black màu của con cờ dùng trong chương trình
+
+TRAIN = TRUE/FALSE: chương trình sẽ (không) training và tạo model để dự đoán
+TEST  = TRUE/FALSE: chương trình sẽ (không) test dựa trêm model lưu trong biến MODEL_PATH
+
+Ví dụ:
+MODEL_CLASS = "Net2"
+COLOR = "black"
+TRAIN = True
+	MODEL_PATH = "b.pth"
+	EPOCH = 10
+TEST = True
+
+sẽ training mạng CNN với:
+- Model sử dụng class Net2
+- color là black nên sử dụng thư mục ./data_black để train và ./data_black_test để test
+
+- TRAIN = True : chạy chương trình training với EPOCH = 10 và lưu vào file "b.pth"
+- TEST = True: chạy chương trình test với một số ảnh
--- Python/cnn/train.py
+++ Python/cnn/train.py
@@ -40,12 +40,50 @@ class Net(nn.Module):
         output = self.out(x)
         return output, x           # return x for visualization
 
+class Net2(nn.Module):
+    def __init__(self):
+        super(Net2, self).__init__()
+
+        self.conv1 = nn.Sequential(         # input shape (1, 90, 90)
+            nn.Conv2d(
+                in_channels=1,              # input height
+                out_channels=16,            # n_filters
+                kernel_size=3,              # filter size
+                stride=1,                   # filter movement/step
+                padding=2,                  # if want same width and length of this image after Conv2d, padding=(kernel_size-1)/2 if stride=1
+            ),                              # output shape (16, 90, 90)
+            nn.BatchNorm2d(16),
+            nn.ReLU(),                      # activation
+            nn.MaxPool2d(kernel_size=2),    # choose max value in 2x2 area, output shape (16, 45, 45)
+        )
+        self.conv2 = nn.Sequential(         # input shape (16, 45, 45)
+            nn.Conv2d(16, 32, 3, 1, 1),     # output shape (32, 45, 45)
+            nn.BatchNorm2d(32),
+            nn.ReLU(),                      # activation
+            nn.MaxPool2d(2),                # output shape (32, 23, 23)
+        )
+        self.out = nn.Linear(
+            in_features=(32 *23 *23),
+            out_features=7
+        )   # fully connected layer, output 7 classes
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = x.view(x.size(0), -1)  # flatten the output of conv2 to (batch_size, 32 * 7 * 7)
+        output = self.out(x)
+        return output, x           # return x for visualization
+
 
 TRAN_TO_TENSOR = transforms.ToTensor()
 LABEL_MAPS = ["Cannon", "Chariot", "Elephant", "General", "Guard", "Horse", "Soldier"]
 
 def load_cnn(path):
     model = Net()
+    model.load_state_dict(torch.load(path))
+    return model
+def load_cnn2(path):
+    model = Net2()
     model.load_state_dict(torch.load(path))
     return model
 
@@ -58,21 +96,21 @@ def predict(model, cvimage):
 
 if __name__ == '__main__':
 
-    # red black
-    COLOR = "black"
+    TRAIN = True # True False
+    TEST  = True # True False
 
-    # True False
-    TRAIN = False
-    TEST  = True
+    COLOR       = "black" # red black
+    MODEL_CLASS = "Net2" # Net2 Net
 
+    
     if COLOR == "red":
-        MODEL_PATH = 'red1.pth'
+        MODEL_PATH = 'redA.pth'
         TEST_SIZE = 18
-        EPOCH = 20
+        EPOCH = 10
     else: 
-        MODEL_PATH = 'black1.pth'
+        MODEL_PATH = 'blackA.pth'
         TEST_SIZE = 17
-        EPOCH = 20
+        EPOCH = 10
 
     DATA_PATH = "data_" + COLOR
     TEST_PATH = "data_" + COLOR + "_test/"
@@ -80,20 +118,27 @@ if __name__ == '__main__':
     if TRAIN:
 
         trainset = torchvision.datasets.ImageFolder(
-            root = DATA_PATH,
+            root = DATA_PATH, #đường dẫn tới thư mục chứa ảnh để training
             transform = transforms.Compose(
                         [transforms.Grayscale(num_output_channels=1),
                          transforms.Resize([90,90]),
-                         transforms.ToTensor()])
+                         transforms.ToTensor()]) #giai đoạn tiền xử lý ảnh để training: gồm đọc ảnh dạng Gray ==> resize thành 90x90 ==> chuyển thành kiểu dữ liệu Tensor
         )
+
         trainloader = torch.utils.data.DataLoader(
             trainset,
             batch_size=128,
-            num_workers=2,
-            shuffle=True
-        )
+            num_workers=2, #dùng nhiều thread để đọc ảnh, ko cần quan tâm
+            shuffle=True #random ảnh cho quá trình training
+        ) #mô tả cách lấy dữ liệu training để mạng CNN học
 
-        net = Net()
+        #load mạng CNN từ Net/Net2 Class
+        if MODEL_CLASS == "Net":
+            net = Net()
+        else:
+            net = Net2()
+
+        #Định nghĩa phương pháp tính sai số, tối ưu, learning ...
         criterion = nn.CrossEntropyLoss()
         optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
@@ -108,8 +153,8 @@ if __name__ == '__main__':
                 optimizer.zero_grad()
 
                 # forward + backward + optimize
-                outputs, _  = net(inputs)
-                loss = criterion(outputs, labels)
+                outputs, _  = net(inputs) #tính toán "dự đoán" output ứng với dữ liệu inputs
+                loss = criterion(outputs, labels) #tính toán loss
                 loss.backward()
                 optimizer.step()
 
@@ -117,11 +162,14 @@ if __name__ == '__main__':
                 running_loss += loss.item()
                 print(i)
 
-        torch.save(net.state_dict(), MODEL_PATH)
+        torch.save(net.state_dict(), MODEL_PATH) #lưu model train đc vào file
         del net
 
     if TEST:
-        net = Net()
+        if MODEL_CLASS == "Net":
+            net = Net()
+        else:
+            net = Net2()
         net.load_state_dict(torch.load(MODEL_PATH))
 
         for i in range(1,TEST_SIZE+1):
