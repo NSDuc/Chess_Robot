3e38e53414377c241346696295ec3e71af9d583f
NSDuc <nguyensyduc97@gmail.com>
2020-11-19 01:06:25 +0700

    fix bug

--- Python/catchroi.py
+++ Python/catchroi.py
@@ -101,43 +101,66 @@ def detect_roi(original):
   posY_list  = []
   b,g,r = cv2.split(original)
   recognition = (0.1*r + 0.1*g + 0.8*b).astype(np.uint8)
-  blurred = gaussian(recognition, sigma=.8)
+  # imshow('recognition',cv2.resize(recognition, (640,590)))
   thresh = threshold_otsu (recognition)
-  _ , bw = cv2.threshold (recognition, thresh*0.72, 255, cv2.THRESH_BINARY_INV)
+  level  = int(thresh*0.72)
+  _ , bw = cv2.threshold (recognition, level, 255, cv2.THRESH_BINARY_INV)
 
-  #bw = 255 - bw
-  # cv2.imshow('bw',cv2.resize(bw, (640,590)))
-  # cv2.waitKey(0)
-  # cv2.destroyAllWindows()
+  # imshow('bw',cv2.resize(bw, (640,590)))
+
   se = np.array([[0,0,1,0,0],
                  [0,1,1,1,0],
                  [1,1,1,1,1],
                  [0,1,1,1,0],
                  [0,0,1,0,0]], np.uint8)
-  openbw = cv2.morphologyEx(bw, cv2.MORPH_CLOSE, se)
 
-  labels = measure.label(openbw)
+
+  openbw = cv2.morphologyEx(bw, cv2.MORPH_CLOSE, se)
+  
+  se = np.array([[0,1,0],
+                 [1,1,1],
+                 [0,1,0]], np.uint8)
+  regionprops_bw = cv2.erode(openbw, se, iterations = 1)
+
+  imshow('openbw',cv2.resize(openbw, (640,590)))
+
+  # labels = measure.label(openbw)
+  labels = measure.label(regionprops_bw)
   props  = measure.regionprops(labels)
 
+  num_chess = 0
   for prop in props:
     minr, minc, maxr, maxc = prop.bbox
     width  = maxr - minr
     length = maxc - minc
-    diff_wl = np.abs(width-length)
+    mean = openbw[minr:maxr, minc:maxc].mean(axis=0).mean(axis=0)
 
-    if (diff_wl <= 25) and (width>=75) and (width<=120) and (length>=75) and (length<=120):
-      # centerX = (minr + maxr)/2
-      # centerY = (minc + maxc)/2
+    if ((width/length > 0.8) and (width/length) < 1.2) and \
+    ((width>=70) and (width<=135)) and ((length>=70) and (length<=135)) \
+    and (mean > 50):
       centerX = prop.centroid[1]
       centerY = prop.centroid[0]
-      subimg = original[minr:maxr, minc:maxc].copy()
+      centerX, centerY = int(centerX), int(centerY)
+
+      if (length - (centerX - minc)*2) > 5:
+        length = (centerX - minc)*2 + 1
+      if (width - (centerY - minr)*2) > 5:
+        width = (centerY - minr)*2 + 1
+      # subimg  = original[minr:maxr, minc:maxc].copy()
+      subimg = original[minr:minr+width, minc:minc+length]
       posX_list.append(centerX)
       posY_list.append(centerY)
       chess_list.append(subimg)
-      # cv2.imshow('subimg', subimg)
-      # cv2.waitKey(0)
-      # cv2.destroyAllWindows()
 
+      num_chess += 1
+      imshow('subimg', subimg)
+      # subimg2[centerY-minr-2:centerY-minr+2, centerX-minc-2: centerX-minc+2] = (255,0,0)
+      # imshow('subimg2', subimg2)
+    # elif (width > 30) and (length > 30):
+    #   print(mean)
+    #   imshow('subimg_2',original[minr:maxr, minc:maxc])
+
+  print("number of chess:", num_chess)
   posX = np.array(posX_list).T
   posY = np.array(posY_list).T
 
@@ -183,9 +206,25 @@ def detect_turned_roi(frame):
   return DY1+y, DX1+x, roi
 
 def test():
-  picname = r"F:\nntest\testimg\Picture 205.jpg"
-  img = cv2.imread(picname)
-  detect_roi(img)
+  picnames = [
+    r"F:\nntest\testimg\Picture 100.jpg",
+    r"F:\nntest\testimg\Picture 101.jpg",
+    r"F:\nntest\testimg\Picture 160.jpg"
+    # r"F:\nntest\testimg\Picture 1001.jpg",
+    # r"F:\nntest\testimg\Picture 1002.jpg",
+    # r"F:\nntest\testimg\Picture 1003.jpg",
+    # r"F:\nntest\testimg\Picture 1004.jpg",
+    # r"F:\nntest\testimg\Picture 1005.jpg",
+    # r"F:\nntest\testimg\Picture 1006.jpg",
+    # r"F:\nntest\testimg\Picture 1007.jpg"
+  ]
+  for picname in picnames:
+    img = cv2.imread(picname)
+    openbw, chess_list, posX_sort, posY_sort = detect_roi(img)
+    for raw_chess in chess_list:
+      label, bw_chess = ConvoNN.ConvoNN(raw_chess)
+      imshow("bw_chess", bw_chess)
+      print(label)
 
   # picname = r"F:\nntest\testimg\Picture 101.jpg"
   # img = cv2.imread(picname)
@@ -200,21 +239,31 @@ def test_detect_turned_roi():
     r"F:\nntest\testimg\Picture 183.jpg",
     r"F:\nntest\testimg\Picture 184.jpg",
     r"F:\nntest\testimg\Picture TC_11.jpg",
-    r"F:\nntest\testimg\Picture TC_12.jpg"]
+    r"F:\nntest\testimg\Picture TC_12.jpg"
+    r"F:\Outsource\Bug\Err8.jpg",
+    r"F:\Outsource\Bug\Err9.jpg"
+  ]
   for picname in picnames:
     frame = cv2.imread (picname)
-    roi = detect_turned_roi(frame)
+    _, _, roi = detect_turned_roi(frame)
     imshow('roi', roi)
 
     label, bw_chess = ConvoNN.ConvoNN(roi)
     print(label)
 
 
+def im2bw(image, level):
+  bw = np.zeros(image.shape)
+  for i in range(bw.shape[0]):
+    for j in range(bw.shape[1]):
+      if(image[i,j] >= level):
+        bw[i,j] = 255
+  return bw
 
 def imshow(name, image):
   cv2.imshow(name, image)
   cv2.waitKey(0)
   cv2.destroyWindow(name)
 
-# test()
+test()
 # test_detect_turned_roi()
